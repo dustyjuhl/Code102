1cfc5f5e51b6ccbb1a8a7cf640d34bcca3c0f436
Dusty Juhl <dustyjuhl@gmail.com>
2019-10-26 11:57:27 -0500

    Variable concatenation examples with math

--- app.js
+++ app.js
@@ -17,17 +17,36 @@ function draw() {
 
 //Declare a function
 function day1AM() {
-    console.log("Hello world");
+    // console.log("Hello world");
     var firstName = "Dusty";
     var lastName = "Juhl";
     var age = 42;
+    var heightInInches = 69;
     var address = "1600 NW Ridge Rd";
     var city = "Ankeny";
     var state = "IA";
     var zipCode = "50023";
-    console.log (typeof(hasTattoos));
+    // console.log(typeof(hasTattoos));
     var likesPizza = true;
     var hasTattoos = false;
+    console.log(age);
+    age = age + 1;
+    console.log(age);
+    age++;
+    console.log(age);
+    age += 1;
+    console.log(age);
+    age /= 5;
+    console.log(age);
+    console.log(firstName + " " + lastName);
+    console.log(firstName + " " + lastName + " is " + age + " years old.")
+    console.log("Do you like pizza? " + likesPizza);
+    var fullName = firstName + " " + lastName;
+    console.log(fullName);
+    var feet = Math.floor(heightInInches / 12);
+    console.log(feet);
+    var inches = heightInInches % 12;
+    console.log(fullName + " is " + feet + " feet " + inches + " inches tall.")
 }
 
 //Call a function
